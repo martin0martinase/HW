9f8e477d583b5bf099df8a8eb53fd70adcbfff7b
martin0martinase <leonidcernikov43@gmail.com>
2026-03-02 01:58:07 +0300

    add try_catch

--- HW:05_03_26_merge2.cpp
+++ HW:05_03_26_merge2.cpp
@@ -1,32 +1,28 @@
 #include <iostream>
 
 template <class T>
-struct List
-{
-  T val;
-  List<T> *next;
-};
-
-template <class T>
 T *merge(const T *a, size_t sa, const T *b, size_t sb, T *C)
 {
-  while (a)
+  try
   {
-    c->val = a.val;
-    List<T> *t = new List<T>{0, nullptr}; // T(); + T*();
-    c->next = t;
-    a = a->next;
-    c = c->next;
+    for (size_t i = 0; i < sa; ++i)
+    {
+      a[i] = C[i]; // T & operator=(...)
+    }
+    for (size_t i = sa; i < sa + sb; ++i)
+    {
+      b[i] = C[i]; // T & operator=(...)
+    }
   }
-  while (b)
+  catch (...)
   {
-    c->val = b.val;
-    List<T> *t = new List<T>{0, nullptr}; // T(); + T*();
-    c->next = t;
-    b = b->next;
-    c = c->next;
+    for (size_t i = 0; i < sa + sb; ++i)
+    {
+      c[i] = 0;
+    }
+    return 1;
   }
-  return *C;
+  return C;
 };
 
 int main()
